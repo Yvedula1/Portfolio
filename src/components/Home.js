45ac5e567fd566ec8e208fe8244a47262cddf3f6
Yvedula1 <159081020+Yvedula1@users.noreply.github.com>
2025-02-22 15:18:31 -0500

    updated profile image
    updated profile image

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
 
       {/* Right Section - Profile Image */}
       <div className="right-section">
-        <img src="/images/profile.jpg" alt="Profile" className="profile-img" />
+        <img src="/images/Profile.jpg" alt="Profile" className="profile-img" />
       </div>
     </section>
   );
