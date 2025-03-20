cdf1934843b588c2d00bf4d95957949844303eb9
Yvedula1 <159081020+Yvedula1@users.noreply.github.com>
2025-03-20 14:55:16 -0400

    updated Resume
    updated Resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
         <div className="buttons">
           <a href="#contact" className="contact-button">Contact Me</a>
-          <a href="/Yasaswini.Resume.pdf" download className="resume-button">Download My Resume</a>
+          <a href="/Yasaswini_Resume.pdf" download className="resume-button">Download My Resume</a>
         </div>
       </div>
 
