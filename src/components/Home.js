4c67cf90f19ea99689b6e612928704b40ac9c649
Yvedula1 <159081020+Yvedula1@users.noreply.github.com>
2025-08-28 10:35:34 -0400

    home update
    home update

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className="home-container">
       
-        <h1>Hi, I'm <span className="name-highlight">Yasaswini V</span></h1>
+        <h1>Hi, I'm <span className="name-highlight">Yasaswini v</span></h1>
         <p>
 Full Stack Developer with 4+ years of experience building scalable enterprise applications. Skilled in developing ADA-compliant, modular UIs using React.js (Hooks, Context API) and Tailwind CSS. Proficient in designing RESTful microservices with Spring Boot, applying domain-driven design and dependency injection for scalable, interoperable systems. Experienced in handling high-volume transactions with PostgreSQL, MongoDB, and Hibernate, optimizing performance with caching and indexing. Skilled in cloud-native deployments using AWS, Docker, and Kubernetes, with CI/CD pipelines built via GitHub Actions and SonarQube. Collaborative team player, contributing to sprint delivery through Agile workflows using Jira and Confluence.        </p>
 
